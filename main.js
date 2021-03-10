756498082e3cdc3e6ed5b605f61c260e315813c4
roshan0099 <roshan09ka@gmail.com>
2021-03-10 12:13:59 +0530

    fixed bubble sort

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const app = express() 
 
-app.use(express.static(__dirname+'public'))
+
+app.use(express.static(__dirname+'/public'))
 const PORT = process.env.PORT || 8080
 
 app.set('view engine', 'ejs')
 
+
 //home
 app.get("/",(req,res) => {
+	res.send("hello")
+})
+
+//algorithm page
+app.get("/algorithm",(req,res) => {
 	res.render('algorithm')
 } )
+
+
 
 app.listen(PORT, () => {
 	console.log("running on port 8080")

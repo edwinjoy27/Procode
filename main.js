45d6d58c44583cb16403a03329377fc48a553ef2
edwinjoy27 <80337555+edwinjoy27@users.noreply.github.com>
2021-03-10 01:42:38 -0500

    peverfull gui

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express() 
 
-app.use(express.static(__dirname+'public'))
+app.use(express.static(__dirname+'/public'))
 const PORT = process.env.PORT || 8080
 
 app.set('view engine', 'ejs')
 
 //home
 app.get("/",(req,res) => {
-	res.render('algorithm')
+	res.render('home')
 } )
 
 app.listen(PORT, () => {

b5ca71b19b9bcd364c9d93f86731b2b15c618f2d
edwinjoy27 <80337555+edwinjoy27@users.noreply.github.com>
2021-03-11 18:34:34 +0530

    regex

--- public/js/effects.js
+++ public/js/effects.js
@@ -10,7 +10,7 @@ window.onscroll = function() {scrollFunction()};
      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         const header=document.getElementById("header")
         header.style.fontSize = "20px";
-        header.style.opacity = "0.3";
+       
          } else {
                      header.style.fontSize = "60px";
                 }
